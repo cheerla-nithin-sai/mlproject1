47d9b7e0a458670a92dc24b2d84248827f976331
cheerla-nithin-sai <cn11@iitbbs.ac.in>
2024-09-18 14:50:57 +0530

    prediction pipeline

---
+++ app.py
@@ -0,0 +1,52 @@
+## conda activate c:\Users\Admin\Desktop\mlproject1\venv
+
+from flask import Flask,render_template,request
+
+import pandas as pd
+import numpy as np
+
+from src.pipeline.predict_pipeline import CustomData,PredictionPipeline
+
+from sklearn.preprocessing import StandardScaler
+
+# CREATIING APP
+application = Flask(__name__)
+
+app = application
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        # if req is get we will return to home page asking for data
+        return render_template("home.html")
+    
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictionPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")   
+         
+    
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,71 @@
+# 
+import os
+import sys
+from src.exception import CustomException
+from src.utils import load_object
+import pandas as pd
+
+class PredictionPipeline:
+    def __init__(self):
+        pass
+
+    # predict function is to get the model and predict for the selected datapoint
+
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+# here custom data class is responsible for mapping all the inputs that we are giving in html to the backend
+class CustomData:
+    def __init__(  self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int):
+
+        self.gender = gender
+
+        self.race_ethnicity = race_ethnicity
+
+        self.parental_level_of_education = parental_level_of_education
+
+        self.lunch = lunch
+
+        self.test_preparation_course = test_preparation_course
+
+        self.reading_score = reading_score
+
+        self.writing_score = writing_score
+    
+    # above we got all the data and below function is used to convert data to df 
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
